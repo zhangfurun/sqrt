e7fc6bfddfefdaaee13ec3922c7d0c32c013a956
zhangfuurn <122674287@qq.com>
2018-05-15 21:24:43 +0800

    Initial Commit

---
+++ sqrt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sqrt
+//
+//  Created by ifenghui on 2018/5/15.
+//  Copyright © 2018年 ifenghui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
