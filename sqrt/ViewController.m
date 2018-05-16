c4c7361471c0571d1e2d762bb39cff7d5e717fee
zhangfuurn <122674287@qq.com>
2018-05-16 15:17:48 +0800

    Upload Project
    开平方的二分法

--- sqrt/ViewController.m
+++ sqrt/ViewController.m
@@ -16,7 +16,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    CGFloat sqrtNumber = [self sqrtNumber:2];
+    NSLog(@"结果:%lf", sqrtNumber);
+}
+
+- (CGFloat)sqrtNumber:(CGFloat)number {
+    CGFloat sqrtNumber = 0;
+    if (number < 0) {
+        // 这里根据实际情况做处理,简单的返回-1,可以根据-1情况进行处理
+        sqrtNumber = -1;
+    } else {
+        CGFloat oldRangeMid, newRangeMid;
+        CGFloat rangeLeft, rangeRight;
+        rangeLeft = 0;
+        rangeRight = number;
+        newRangeMid = oldRangeMid = (rangeLeft + rangeRight) / 2;
+        do {
+            if (newRangeMid * newRangeMid > number) {
+                rangeRight = newRangeMid;
+            } else {
+                rangeLeft = newRangeMid;
+            }
+            oldRangeMid = newRangeMid;
+            newRangeMid = (rangeRight + rangeLeft) / 2;
+        } while (fabs(newRangeMid - oldRangeMid) > FLT_EPSILON);
+        
+        sqrtNumber = newRangeMid;
+    }
+    
+    return sqrtNumber;
 }
 
 
